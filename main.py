a2c071c4d64339d86d5bd121606a6c221934f240
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-02-04 18:22:59 +0500

    csv fayllar bilan ishlash

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def get_column(data):
     column_name = []
+    s = data.split('\n')
+    
+    for i in s[1:]:
+        l = i.split(',') 
+        column_name.append(l[-1])
+    
     return column_name
 
 data = open('data.csv').read()
-print(type(data))
+print(get_column(data))
